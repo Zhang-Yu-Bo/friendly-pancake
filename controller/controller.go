fb8281f97791ab6e13d8019b24d0fcedb7ef9ff8
Yu-bo, Zhang <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2022-04-10 02:44:44 +0800

    [Create] html to png function

--- controller/controller.go
+++ controller/controller.go
@@ -2,11 +2,13 @@ package controller
 
 import (
 	"fmt"
-	"image"
-	"image/color"
-	"image/png"
+	// "image"
+	// "image/color"
+	// "image/png"
 	"net/http"
 	"runtime"
+
+	wk "github.com/Zhang-Yu-Bo/friendly-pancake/model/wkhtmltoimage"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
@@ -24,37 +26,112 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 
 func RawImage(w http.ResponseWriter, r *http.Request) {
 
-	fileName := r.URL.Query().Get("file_name")
+	// fileName := r.URL.Query().Get("file_name")
 
-	if len(fileName) == 0 {
-		fileName = "image.png"
+	// if len(fileName) == 0 {
+	// 	fileName = "image.png"
+	// }
+
+	// width := 200
+	// height := 100
+
+	// upLeft := image.Point{0, 0}
+	// lowRight := image.Point{width, height}
+
+	// img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+	// // Colors are defined by Red, Green, Blue, Alpha uint8 values.
+	// cyan := color.RGBA{100, 200, 200, 0xff}
+
+	// // Set color for each pixel.
+	// for x := 0; x < width; x++ {
+	// 	for y := 0; y < height; y++ {
+	// 		switch {
+	// 		case x < width/2 && y < height/2: // upper left quadrant
+	// 			img.Set(x, y, cyan)
+	// 		case x >= width/2 && y >= height/2: // lower right quadrant
+	// 			img.Set(x, y, color.White)
+	// 		default:
+	// 			// Use zero value.
+	// 		}
+	// 	}
+	// }
+	// png.Encode(w, img)
+	// // fmt.Fprintf(w, "%s", fileName)
+
+	binPath := "C:\\Users\\Lykoi\\Desktop\\html2image-master\\wkhtmltopdf\\bin\\wkhtmltoimage.exe"
+
+	if runtime.GOOS == "linux" {
+		binPath = "~/bin/wkhtmltoimage"
 	}
 
-	width := 200
-	height := 100
+	html := `
+		<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            body {
+                background-color: #2885D3;
+                /* background-color: rgba(0,0,0,0.2); */
+            }
+            
+            h1 {
+                color: white;
+                text-align: center;
+            }
+            
+            p {
+                color: white;
+                font-family: verdana;
+                font-size: 20px;
+            }
 
-	upLeft := image.Point{0, 0}
-	lowRight := image.Point{width, height}
+            .container {
+                margin: auto;
+                background-color: rgb(21, 23, 24);
+                /* #282c34 */
+                border-radius: 10px;
+                width: 300px;
+                padding: 12px;
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+				word-break: break-word;
+            }
+            .button {
+                width: 12px;
+                height: 12px;
+                border-radius: 50%;
+                margin-right: 5px;
+                display: inline-block;
+            }
+            .title-bar {
+                padding-left: 6px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class='container'>
+            <div class='title-bar'>
+                <span class='button' style='background-color: #ff5f56;'></span>
+                <span class='button' style='background-color: #ffbd2e;'></span>
+                <span class='button' style='background-color: #27c93f;'></span>
+            </div>
+            <h1>My First CSS Example</h1>
+            <p>This is a paragraph.</p>
+        </div>
+    </body>
+</html>`
 
-	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
-
-	// Colors are defined by Red, Green, Blue, Alpha uint8 values.
-	cyan := color.RGBA{100, 200, 200, 0xff}
-
-	// Set color for each pixel.
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			switch {
-			case x < width/2 && y < height/2: // upper left quadrant
-				img.Set(x, y, cyan)
-			case x >= width/2 && y >= height/2: // lower right quadrant
-				img.Set(x, y, color.White)
-			default:
-				// Use zero value.
-			}
-		}
+	c := wk.ImageOptions{
+		BinaryPath: binPath,
+		Input:      "-",
+		HTML:       html,
+		Format:     "png",
 	}
-	png.Encode(w, img)
-	// fmt.Fprintf(w, "%s", fileName)
+
+	if out, err := wk.GenerateImage(&c); err != nil {
+		fmt.Fprintf(w, "Error: %s\n", err.Error())
+	} else {
+		w.Write(out)
+	}
 
 }
