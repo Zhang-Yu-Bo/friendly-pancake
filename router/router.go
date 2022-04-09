009e0c33650d8deadf44262c44589c5500c37b43
Yu-bo, Zhang <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2022-04-10 03:20:04 +0800

    [Update] File Server temp

--- router/router.go
+++ router/router.go
@@ -1,12 +1,16 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/Zhang-Yu-Bo/friendly-pancake/controller"
 	"github.com/gorilla/mux"
 )
 
 func NewRouter() *mux.Router {
 	mRouter := mux.NewRouter()
+
+	mRouter.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(""))))
 
 	mRouter.HandleFunc("/", controller.HomePage).Methods("GET")
 	mRouter.HandleFunc("/raw/image", controller.RawImage).Methods("GET")
