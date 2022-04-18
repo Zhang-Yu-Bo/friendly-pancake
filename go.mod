47df69498e95e423270c7d5a97574e81bdfc1f71
Yu-bo, Zhang <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2022-04-19 02:01:24 +0800

    [Update] CORS Policy

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ module github.com/Zhang-Yu-Bo/friendly-pancake
 go 1.18
 
 require (
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	golang.org/x/time v0.0.0-20220411224347-583f2d630306
 )
+
+require github.com/felixge/httpsnoop v1.0.2 // indirect
--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/Zhang-Yu-Bo/friendly-pancake/router"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
@@ -18,8 +20,16 @@ func main() {
 
 	mRouter := router.NewRouter()
 
+	origins := strings.Split(os.Getenv("ORIGIN_ALLOWED"), ",")
+	if len(origins) == 1 && origins[0] == "" {
+		origins[0] = "*"
+	}
+	originsOk := handlers.AllowedOrigins(origins)
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
 	mServer := &http.Server{
-		Handler:      mRouter,
+		Handler:      handlers.CORS(originsOk, headersOk, methodsOk)(mRouter),
 		Addr:         ":" + port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
